7d7d9b7bd77be11cd628fc504bb895946c7f44e4
Abdullah Muhammad Farouk Hakim <amf.akim@gmail.com>
2021-04-30 13:05:33 +0700

    untracked config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  MONGODB:
-    "mongodb+srv://amfakim:g8RifSmk5v02lLzV@cluster0.d9woo.mongodb.net/projectmanagement?retryWrites=true&w=majority",
-  SECRET_KEY: "some very secret key",
-};
